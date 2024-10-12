50ee6bd5b22e4b1c44fd4a72b4d2126f6622a5c5
SadN00dle <sadnoodle11@gmail.com>
2024-10-12 22:10:26 +0545

    char string initialize

--- hidePass.c
+++ hidePass.c
@@ -1,7 +1,26 @@
 #include <stdio.h>
 
+
 int main()
 {
+    int i = 0;
+    char ch;
+    char pass[20];
+    printf("Enter your password: ");
+
+    scanf("%c", &ch);
+
+    if (ch==13)
+    {
+        pass[i] = '\0';
+        
+    }else
+    {
+        pass[i++] = ch;
+    }
     
+    
+
+
     return 0;
 }
