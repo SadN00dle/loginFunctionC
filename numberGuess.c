87403a4037af2bf4c46ef56c36d0a3d9ccae3f7a
SadN00dle <sadnoodle11@gmail.com>
2024-10-13 12:00:54 +0545

    fixed bug where if number is before string it returns true

--- numberGuess.c
+++ numberGuess.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include <ctype.h>
+#include <string.h>
 
 int randNum;
 int *PrandNum = &randNum;
@@ -20,36 +21,55 @@ void randNumGuess()
     
     while (totalGuess < 6)
     {
-        // char input[10];
-        
+        char input[100];
+       
+
         printf("enter a number between 1 and 10: ");
 
-        if(scanf("%d", &num) != 1)
+        if (fgets(input, sizeof(input), stdin) != NULL)
         {
-            printf("invalid number.Try again!");
+            char *endptr;
+            input[strcspn(input, "\n")] = '\0';
+            num = strtol(input, &endptr, 10);
+
+            if (endptr == input || *endptr != '\0')
+            {
+                printf("Enter a valid number.");
+            }else
+            { 
+                if (num > 10 || num <= 0)
+                {
+                    printf("Please enter a number between 1 and 10.\n");
+                    continue;
+
+                }else if (num == randNum)
+                {
+                printf("you've guessed the correct number!\n");
+                // playerWon = true;
+                break;
+
+                }else
+                {
+                    printf("You lost!\n");  
+                    totalGuess++;
+                } 
+            }
+        }
         
-            while (getchar() != '\n');
-            continue;
-        }
 
+        // if (sscanf(input, "%d", &num) != 1)
+        // {
+        //     printf("Enter a valid number.");
+        // }
 
-        if (num > 10 || num <= 0)
-        {
-            printf("Please enter a number between 1 and 10.\n");
-            continue;
+        // if(scanf("%d", &num) != 1) // to remove the string infinite loop checking return value of scanf
+        // {
+        //     printf("invalid number.Try again!");
+        
+        //     while (getchar() != '\n');
+        //     continue;
+        // }
 
-        }else if (num == randNum)
-        {
-        printf("you've guessed the correct number!\n");
-        // playerWon = true;
-        break;
-
-        }else
-        {
-            printf("You lost!\n");  
-            totalGuess++;
-        }
-     
     }
 }
 
@@ -63,7 +83,7 @@ int main()
     randNumGuess();
 
     printf("The correct number is: %d\n\n", randNum);
-    printf("total guess required: %d\n", totalGuess);
+    printf("total guess: %d\n", totalGuess);
 
     return 0;
 }
