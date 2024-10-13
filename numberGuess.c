c36f74efe586c9187f24e8e9e9880a85acd977cc
SadN00dle <sadnoodle11@gmail.com>
2024-10-13 10:08:20 +0545

    finish numberGuess mini game

--- numberGuess.c
+++ numberGuess.c
@@ -2,8 +2,14 @@
 #include <stdlib.h>
 #include <time.h>
 
+
+
 int randNum;
 int *PrandNum = &randNum;
+
+int num;
+int totalGuess = 0;
+
 void randNumGen(int maxVal)
 {
     srand(time(0));
@@ -12,18 +18,7 @@ void randNumGen(int maxVal)
 
 void randNumGuess()
 {
-
-}
-
-
-int main()
-{
-    int num;
-    int totalGuess = 0;
-
-    randNumGen(10);
-    printf("randNum: %d", randNum);
-
+    
     while (totalGuess < 5)
     {
         printf("enter a number between 1 and 10: ");
@@ -45,6 +40,17 @@ int main()
             totalGuess++;
         }
     }
+}
+
+
+int main()
+{
+    
+    randNumGen(10);
+    printf("randNum: %d", randNum);
+
+    randNumGuess();
+
     printf("The correct number is: %d\n", randNum);
 
     return 0;
