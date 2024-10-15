79b3f5260700eef943a97a42cca02bfd677e2702
SadN00dle <sadnoodle11@gmail.com>
2024-10-15 11:53:08 +0545

    add add function

---
+++ calculator.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int addResult;
+int firstNum;
+int secondNum;
+void add(firstNum, secondNum)
+{
+    addResult = firstNum + secondNum;
+    printf("Addition is: %d", addResult);
+}
+
+
+int main()
+{
+
+    return 0;
+}
